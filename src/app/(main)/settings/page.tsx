5c2bd25e6bce1cbba9c25fb191d1605f9aa3979d
”take2365” <kcsoita.20210268@gmail.com>
2025-01-09 02:07:22 +0900

    設定1

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -1,36 +1,5 @@
-"use client";
+import SettingsLayout from "./layout";
 
-import { supabase } from "../../../../lib/supabase";
-import { useRouter } from "next/navigation";
+const SettingsPage = () => {};
 
-const Logout = () => {
-  const router = useRouter();
-  const handleLogout = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error(error.message);
-    } else {
-      console.log("User logged out");
-      alert("ログアウトしました。");
-      router.refresh();
-      router.push("/");
-    }
-  };
-
-  return (
-    <header>
-      <button onClick={handleLogout}>Logout</button>
-    </header>
-  );
-};
-const settings = () => {
-  return (
-    <div>
-      <h1>設定</h1>
-      <p>メールアドレスやパスワードを変更するところです（理想）</p>
-      <Logout /> {/* ログアウトコンポーネントを追加 */}
-    </div>
-  );
-};
-
-export default settings;
+export default SettingsPage;
